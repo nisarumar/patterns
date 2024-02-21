6e5d5433cc67f2595b314d7c1d701433c4a40032
nisarumar <48697531+nisarumar@users.noreply.github.com>
2024-02-21 16:10:07 +0100

    Update template_type_deduction.cpp

--- template_type_deduction.cpp
+++ template_type_deduction.cpp
@@ -6,8 +6,14 @@ void f(T* param);        // param is now a pointer
 template<typename T>
 void K(T& param);       // param is a reference
 
+template<typename T>
+void L(T&& param);       // param is now a universal reference
+
 int main()
 {
+
+    // Case 1: ParamType is a Reference or Pointer, but not a Universal Reference   
+    
     int x = 27;             // x is an int
     const int *px = &x;      // px is a ptr to x as a const int, deduced type is 
 
@@ -28,6 +34,19 @@ int main()
 
     K(rx);                  // T is const int,
                         // param's type is const int&
+
+    //Case 2: ParamType is a Universal Reference
+    L(x);                    // x is lvalue, so T is int&,
+                         // param's type is also int&
+
+    L(cx);                   // cx is lvalue, so T is const int&,
+                             // param's type is also const int&
+
+    L(rx);                   // rx is lvalue, so T is const int&,
+                             // param's type is also const int&
+
+    L(27);                   // 27 is rvalue, so T is int,
+                             // param's type is therefore int&&
     return 0;
 }
 
@@ -42,4 +61,10 @@ template<typename T>
 void K(T& param)
 {
     std::cout<<"Val_K: "<<param<<std::endl;
+}
+
+template<typename T>
+void L(T&& param)
+{
+    std::cout<<"Val_L: "<<param<<std::endl;
 }
