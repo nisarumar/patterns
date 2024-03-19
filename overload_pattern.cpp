39e32465027939328771aad3a9a7f9b35a73ac92
nisarumar <48697531+nisarumar@users.noreply.github.com>
2024-03-19 15:01:02 +0100

    Create overload_pattern.cpp

---
+++ overload_pattern.cpp
@@ -0,0 +1,34 @@
+#include<variant>
+#include<string>
+
+template<typename ... Ls>
+struct over_load: Ls ... {
+    using Ls::operator()...;
+};
+
+
+std::variant<int, std::string, double> var;
+
+#include <iostream>
+
+int main(){
+
+    over_load overload {
+        [](int &val){std::cout<<"I am an int\n";},
+        [](std::string &val){std::cout<<"I am a string\n";},
+        [](double &val){std::cout<<"I am a double\n";}
+    };
+    
+    var = "We will will rock you\n";
+    overload(std::get<1>(var));
+
+    var = 10;
+    overload(std::get<0>(var));
+
+    var = 10.0;
+    overload(std::get<double>(var));
+
+    std::visit(overload, var);
+
+  return 0;
+}
